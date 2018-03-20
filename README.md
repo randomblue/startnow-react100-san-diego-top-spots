0bda1ae6c309ba91d1d614a42318195d2491d0d5
randomblue <jpmedallon@gmail.com>
2018-03-19 17:40:21 -0700

    React 100 - San Diego Top Spots

---
+++ README.md
@@ -0,0 +1,10 @@
+# 'React - San Diego Top Spots'
+
+## 'Using JSON with React'
+
+```
+git clone https://github.com/randomblue/startnow-react100-san-diego-top-spots.git
+cd startnow-react100-san-diego-top-spots
+npm install
+npm start
+```
---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='cointainer'>
+        <h1> San Diego Top Spots</h1>
+        
+        <div>A list of 30 places to see in San Diego, California</div>
+
+        <div className='App'></div>
+      </div>  
+    );
+  }
+}
+
+export default App;
